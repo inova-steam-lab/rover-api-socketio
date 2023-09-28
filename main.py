0b74e494939d57c1a0c97421ca979439c68a98e2
christian <christianoliveira1908@hotmail.com>
2023-09-28 11:20:23 -0300

    Integração do socketio com o FastAPI.

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+import socketio
+
 
 app = FastAPI()
 
+# Ref: https://www.reddit.com/r/FastAPI/comments/neds9c/integrate_socketio_with_fastapi/
+#Estabelece a integração entre o FastAPI e o SocketIO.
+socket = socketio.AsyncServer(cors_allowed_origins='*', async_mode='asgi')
+socketio_app = socketio.ASGIApp(socket, app)
+
 # Serve os arquivos que estão na pasta /static
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 @app.get("/")
 def index():
     ''' Página inicial que será utilizada para controlar o Hover. '''
     return FileResponse('static/index.html')
+
+
+@socket.event
+def connect(sid, environ):
+    print("connect ", sid)
